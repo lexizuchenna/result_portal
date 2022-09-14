cdf62e3ab98ae6dff15bd9924e9b53c18f275ed0
Alexander Ukwueze <lextechspec@gmail.com>
2022-09-14 09:28:35 +0100

    host

--- public/js/admin-result.js
+++ public/js/admin-result.js
@@ -1,26 +1,26 @@
-const approval = document.getElementsByClassName('result')
+const approval = document.getElementsByClassName("result");
+const host = document.getElementById("host").innerText;
 for (i = 0; i < approval.length; i++) {
-    // console.log(approval[i].getAttribute('name'))
-    approval[i].addEventListener('change', (e) => {
-        let result = e.target
-        let id = result.getAttribute('name')
+  // console.log(approval[i].getAttribute('name'))
+  approval[i].addEventListener("change", (e) => {
+    let result = e.target;
+    let id = result.getAttribute("name");
 
-            axios
-            .post(
-              `http://localhost:2022/users/api/admin/approve`,
-              {
-                  resultId: id,
-                    approval: result.checked,
-              },
-              {
-                headers: {
-                  "Content-Type": "application/json",
-                },
-              }
-            )
-            .then((res) => {
-                console.log(res)
-            })
-        
-    })
+    axios
+      .post(
+        `${host}/users/api/admin/approve`,
+        {
+          resultId: id,
+          approval: result.checked,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+      });
+  });
 }
